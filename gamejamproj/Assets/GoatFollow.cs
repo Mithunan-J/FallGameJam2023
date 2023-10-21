087f0ff5d22028b2b4bfc6213f019e60396df99c
Mithunan-J <mithunanjay@gmail.com>
2023-10-21 14:12:53 -0400

    Goat Following Mechanic

---
+++ gamejamproj/Assets/GoatFollow.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoatFollow : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+    public float distance;
+    public float stoppingDistance = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+        
+        //Might remove later
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        ///end
+
+        
+
+        if(distance > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.fixedDeltaTime);
+            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        }
+    }
+}
